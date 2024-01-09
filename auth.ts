cff8b8c49e6e95898f5c37a7f26fa23f3fbf04aa
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-01-09 08:39:56 -0300

    fix: fixing type errors from typescript auth.ts session user and token

--- auth.ts
+++ auth.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
+import { UserRole } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
+
+//Adicionando novos campos ao tipo user do session
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +13,28 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+
+      if (session.user && token.role) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
